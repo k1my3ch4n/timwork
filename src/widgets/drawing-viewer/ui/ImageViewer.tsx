81191c87af1720340aff6981c397746f838ad5f9
Kim <kevin2695@naver.com>
2026-02-27 14:53:41 +0900

    feat : 이미지에 zoom pinch 적용한 ImageViewer 생성

---
+++ src/widgets/drawing-viewer/ui/ImageViewer.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { TransformWrapper, TransformComponent } from 'react-zoom-pan-pinch';
+import { useDrawingStore, useChildDrawings } from '@entities/drawing';
+import { PolygonOverlay } from '@features/polygon-overlay';
+
+interface ImageViewerProps {
+  src: string;
+  alt: string;
+}
+
+export default function ImageViewer({ src, alt }: ImageViewerProps) {
+  const childDrawings = useChildDrawings();
+  const selectDrawing = useDrawingStore((store) => store.selectDrawing);
+
+  const [imageSize, setImageSize] = useState<{
+    width: number;
+    height: number;
+  } | null>(null);
+
+  const handleImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    const img = e.currentTarget;
+    setImageSize({ width: img.naturalWidth, height: img.naturalHeight });
+  };
+
+  return (
+    <div className="relative h-full">
+      <TransformWrapper initialScale={1} minScale={0.5} maxScale={4} centerOnInit centerZoomedOut>
+        <TransformComponent>
+          <div className="relative inline-block">
+            <img src={src} alt={alt} onLoad={handleImageLoad} />
+            {imageSize && childDrawings.length > 0 && (
+              <PolygonOverlay
+                children={childDrawings}
+                imageWidth={imageSize.width}
+                imageHeight={imageSize.height}
+                onSelect={selectDrawing}
+              />
+            )}
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
+}
